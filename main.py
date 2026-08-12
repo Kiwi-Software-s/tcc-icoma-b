4576d7ee7da39df7a9b8b2474ecb52c4de83d625
Vitor Bispo <vitorhugobispo20@gmail.com>
2026-08-12 17:32:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
 
-from flask import Flask, render_template
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    nome = "Icoma.com"
+    nome = 'icoma.com.br'
     return render_template('index.html', site = nome)
 
 @app.route("/login")
 def login():
     return render_template('login/login.html')
+
 def main():
     app.run(host="0.0.0.0", port = int(os.environ.get("PORT", 10000)))
 
