3ccea5a11faddd864b3daf02af476c7e0fb66042
Vitor Bispo <vitorhugobispo20@gmail.com>
2026-08-12 17:07:22 -0300

    Portas de Render da Esteira
    Correção do Render

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def index():
 def login():
     return render_template('login/login.html')
 def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+    app.run(host="0.0.0.0", port = int(os.environ.get("PORT", 10000)))
 
 if __name__ == "__main__":
     main()
